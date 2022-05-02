a3d2e21583751cf104a9df1d38cf8103f21b91a2
ApollosSevere <severeapollos@gmail.com>
2022-05-02 14:29:10 -0400

    .

--- server/db/db.js
+++ server/db/db.js
@@ -8,24 +8,10 @@ const config = {
   logging: false,
 };
 
-const dbConfig = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234",
-  DB: "issuetracker",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
 if (process.env.LOGGING === "true") {
   delete config.logging;
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
 if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
@@ -34,21 +20,8 @@ if (process.env.DATABASE_URL) {
   };
 }
 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-//   config
-// );
-
-const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  logging: false,
-  operatorsAliases: false,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+);
 module.exports = db;
