caf8a0893be6bfb70fdbebc1cc3ab65711c9d45a
ApollosSevere <severeapollos@gmail.com>
2022-05-02 15:04:47 -0400

    .

--- server/db/db.js
+++ server/db/db.js
@@ -9,10 +9,10 @@ const config = {
 };
 
 const dbConfig = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234",
-  DB: "issuetracker",
+  HOST: process.env.HOST || "localhost",
+  USER: process.env.USER || "postgres",
+  PASSWORD: process.env.PASSWORD || "1234",
+  DB: process.env.DATABASE_URL || "issuetracker",
   dialect: "postgres",
   pool: {
     max: 5,
